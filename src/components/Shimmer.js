43aa045d9e3fb5eb97ea97c6280d5ef849d8e582
Vakil Verma <vakilverma7@gmail.com>
2025-07-31 07:29:34 +0530

    shimmer css fixes

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Shimmer = () => {
   return (
-    <div className="shimmer-parent">
+    <div className="restrauntList">
       {Array(20)
         .fill("")
         .map(() => {
